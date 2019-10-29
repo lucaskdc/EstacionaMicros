33a11668cf7122c84576dd5eb1d06e8938cdba2e
Lucas K. Dal Castel <lucas.dalcastel@hotmail.com>
2019-10-29 10:41:30 -0300

    Criando arquivos base do projeto no AVR Studio

---
+++ AVREstacionaMicros/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * AVREstacionaMicros.cpp
+ *
+ * Created: 29/10/2019 10:36:24
+ * Author : Lucas
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
