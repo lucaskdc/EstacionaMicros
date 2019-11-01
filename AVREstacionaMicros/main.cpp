686bdee0fb4244928ccaaccdc82a039f10066697
Lucas K. Dal Castel <lucas.dalcastel@hotmail.com>
2019-10-31 21:25:19 -0300

    Mudando coisas

--- AVREstacionaMicros/main.cpp
+++ AVREstacionaMicros/main.cpp
@@ -18,18 +18,17 @@ void cancelaAbre(char entradaSaida);
 
 char desbloqueado=0;
 char cancelaAberta=0;
-
+char estado=0, estadoAnterior=-1;
 DataHora tempoAtual;
 
-Veiculo carros[256];
+Veiculo carros[120]; //máximo 120 carros no estacionamento
 
 
 int main(void)
 {
     /* Replace with your application code */
 	char byteLido;
-	char vetor[64];
-
+	char vetor[64];	
 	
 	DDRB = 1<<7;
 	
@@ -47,26 +46,75 @@ int main(void)
 					case 'B': //Bloqueia
 						bloqueia();
 						escreveVetor("EB",2);
-						break;
+						break; //B
 					case 'D': //Desbloqueia
 						desbloqueia();
 						escreveVetor("ED", 2);
-						break;
+						break; //D
 					case 'H': //Recebe DataHora
 						leVetor(vetor, 4);
 						tempoAtual.setByVector(vetor);
-						break;
+						break; //H
 					case 'N': //Novo Carro
 						byteLido=le(); //1 entrada, 2 saída
 						leVetor(vetor, 8); //Lê placa 7char+\0
 						escreveVetor("EN", 2); //Resposta para Servidor
-						if(desbloqueado)
-							cancelaAbre(byteLido);
-						break;
+						switch(byteLido){
+							case '1':
+								carros[posCarro].estado = NAENTRADA;
+								if(desbloqueado)
+									cancelaAbre(byteLido);
+								else{
+									LCD::limpa();
+									LCD::print("Bloqueado");
+								}
+								
+								break;
+							case '2':
+								carros[posCarro].estado = NASAIDA;
+								if(desbloqueado){
+									if(carros[posCarro].dataSaidaPaga.diffMin(tempoAtual) > 0){
+										LCD::limpa();
+										LCD::print("Realize o Pagamento");
+									}else{
+										cancelaAbre(byteLido);
+									}
+								}else{
+									LCD::limpa();
+									LCD::print("Bloqueado");
+								}
+								break;
+						}
+						break; //N
+					case 'S': //Carro saindo
+						byteLido = le(); //1 entrada, 2 saída
+						leVetor(vetor,8);
+						escreveVetor("ES", 2);
+						char posCarro = procuraPlaca(vetor);
+						if(posCarro == -1){
+							posCarro = procuraPlaca("AAA0000");
+						}
+						switch(byteLido){
+							case '1':
+								carros[posCarro].estado = DENTRO;
+								break;
+							case '2':
+								carros[posCarro].estado = FORA;
+								break;
+						}
 						
-				}			
-			}
+						cancelaFecha(byteLido);
+						break; //S
+				}//switch
+			}//if bytelido == 'S'
+		}// if novoDado
+		
+		switch(estado){
+			case ESTADOINICIAL:
+				break;
+			case 
 		}
+		
 		escreveVetor("EO", 2);
 		atrasoms(100);
     }
@@ -74,26 +122,31 @@ int main(void)
 
 void bloqueia(){
 	//DESLIGADO NA TELA
+	//16 - 9 = 7 -> esceve na coluna 4
+	LCD::limpa();
+	LCD::printPos("Desligado", 1, 4);
+	cancelaFecha(1);
+	cancelaFecha(2);
 	desbloqueado = 0;
 }
 
 void desbloqueia(){
-
 	desbloqueado = 1;
 }
 
-void cancelaAbre(char entradaSaida){
-	char vetorSerial[2];
+void cancelaAbre(char entradaSaida){ //'E','A',n
 	escreveVetor("EA", 2);
 	escreve(entradaSaida);
-	leVetor(vetorSerial, 2);
+	atrasoms(2);
+	verificaResposta("SA", 2, "Erro Abertura Cancela");
 }
 
-void cancelaFecha(char entradaSaida){
+void cancelaFecha(char entradaSaida){ //'E','F',n
 	char vetorSerial[2];
 	escreveVetor("EF", 2);
 	escreve(entradaSaida);
-	leVetor(vetorSerial, 2);
+	atrasoms(2);
+	verificaResposta("SF", 2, "Erro Fechamento Cancela");
 }
 
 
@@ -106,4 +159,26 @@ char *enviaCartao(char vetorDados[]){
 	leVetor(resposta, 3);
 	leVetor(resposta, resposta[2]);
 	return resposta;
+}
+
+void erro(char vetor[]){ //Imprime erro e dá pausa de 1s.
+	LCD::limpa();
+	LCD::print(vetor);
+	atrasoms(1000);
+}
+
+void verificaResposta(char respostaEsperada[], char n, char mensagem[]){
+	for(int i=0; i<n; i++){
+		if(le() != respostaEsperada){
+			erro(mensagem);
+		}
+	}
+}
+
+char procuraPlaca(char placaProcurada[]){
+	for(int i=0; i<120; i++){
+		if(carros->ehPlacaIgual(placaProcurada))
+			return i;
+	}
+	return -1;
 }
